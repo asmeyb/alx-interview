abbf0cc18c288fb79ac1ff7dac4258e2332df02a
root <Asmamaw Yismaw>
2022-05-18 11:44:09 -0700

    Starwars Characters API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,27 @@
 #!/usr/bin/node
-/* Star Wars Characters - Using the request module */
+
 const request = require('request');
-const urlApi = 'https://swapi-api.hbtn.io/api/films/';
-const movieId = process.argv[2];
-// query API
-request(urlApi + movieId, (error, response, body) => {
-  if (error) throw error;
-  const characters = JSON.parse(body).characters;
-  showNames(characters);
-});
-// show results on the console
-const showNames = (names, i = 0) => {
-  if (i === names.length) return;
-  request(names[i], (error, response, body) => {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    showNames(names, i + 1);
+
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
   });
 };
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
